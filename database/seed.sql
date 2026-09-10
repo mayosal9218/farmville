2de2c178e02ea5f2ca71b21cfd0b53b44efbaef5
Salami <mayowaolanrewajusalami@gmail.com>
2026-09-10 15:47:41 -0700

    Add FarmLedger database files

---
+++ database/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO farms(name,owner_name) VALUES ('Green Valley Farm','Alex Farmer');
+INSERT INTO fields(farm_id,name,area_acres) VALUES (1,'North Field',12.5),(1,'Orchard',4);
+INSERT INTO activity_types(name) VALUES ('Planting'),('Watering'),('Harvesting'),('Animal check'),('Equipment service'),('Fence repair');
